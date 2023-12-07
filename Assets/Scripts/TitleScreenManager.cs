2786c927a159f93213aa4aa2dae80c43314ee598
Yinshi <cissback@hotmail.fr>
2023-12-07 07:12:25 +0100

    Press any..... Or just Enter

--- Assets/Scripts/TitleScreenManager.cs
+++ Assets/Scripts/TitleScreenManager.cs
@@ -5,21 +5,27 @@ using UnityEngine.InputSystem;
 
 public class TitleScreenManager : MonoBehaviour
 {
+    public PlayerInputActions input = null;
     // Start is called before the first frame update
     void Awake()
     {
+        input = new PlayerInputActions();
     }
 
     void OnEnable()
     {
+        input.Player.Any.performed += OnAnyKeyPressed;
+        input.Enable();
     }
     void OnDisable()
     {
-
+        input.Player.Any.performed -= OnAnyKeyPressed;
+        input.Disable();
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private void OnAnyKeyPressed(InputAction.CallbackContext value)
     {
+        Debug.Log("A key waas pressed : ");
+        Debug.Log(value.control.name);
     }
 }
