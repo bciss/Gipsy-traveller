8ca35ac3c632a4b5a73402c70eee8258ed206c1f
Yinshi <cissback@hotmail.fr>
2023-12-07 07:55:37 +0100

    Settings Asset setup GUI=>GUISettings

---
+++ Assets/Saved Settings/Core/Scripts/GUI/SaveSettingsButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GUISettings
+{
+    /// <summary>
+    /// Syncs the button to saving the settings.
+    /// </summary>
+    [RequireComponent(typeof(Button))]
+    public class SaveSettingsButton : MonoBehaviour
+    {
+        void Start()
+        {
+            GetComponent<Button>().onClick.AddListener(delegate { SettingsHelper.Save(); });
+        }
+    }
+}
